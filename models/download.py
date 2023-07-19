77958a01252011f5cbd2ca8716f3fb118bd8f6bc
sasan <74091294+virnow@users.noreply.github.com>
2023-07-19 20:41:29 +0330

    Add files via upload

--- models/download.py
+++ models/download.py
@@ -1,13 +1,12 @@
-import requests
-import random
+from requests import get
+from random import randint
 
 def download_with_progress(url):
-    response = requests.get(url, stream=True)
+    response = get(url, stream=True)
     response.raise_for_status()
     file_name = url.split('/')[-1]
     file_size = int(response.headers.get('content-length', 0))
-    chunk_size = 8192
-    downloaded_size = 0
+    chunk_size,downloaded_size = 8192,0
     print("\033[1;34;40m","Downloading: " + file_name,"\033[0m")
     print("\033[1;32;40m","Size: " + str(file_size // 1024 // 1024) + " MB","\033[0m")
     with open(file_name, "wb") as file:
@@ -15,6 +14,5 @@ def download_with_progress(url):
             if chunk:
                 file.write(chunk)
                 downloaded_size += len(chunk)
-                progress = int(downloaded_size / file_size * 100)
-                print("\033[1;"+str(random.randint(31,37))+";40m",f"Download progress: {progress}%","\033[0m", end="\r")
+                print("\033[1;{};40m".format(randint(31,37)),"Download progress: {}%".format(int(downloaded_size / file_size * 100)),"\033[0m", end="\r")
     return file_name
